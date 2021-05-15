635b2228a848916cceb1e1bf27dd2663c287d5af
Leonardo Machado Leite <leomleite@hotmail.com>
2021-05-15 09:07:33 -0300

    Fixes in main program and all training logs created

---
+++ Serie_Temporal_FPE.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 18:38:32 2021
+
+@author: leoml
+"""
+import sys
+import warnings
+warnings.filterwarnings('ignore')
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Connect the path with your '.env' file name
+load_dotenv(find_dotenv())
+
+home_dir = os.getenv("HOME_DIR")
+
+sys.path.append(home_dir + '\\classes')
+
+import pandas as pd
+import numpy as np
+
+from sarimax_constructor import SARIMAX_Constructor
+from holt_constructor import Holt_Constructor
+from hill_climb import HillClimbOptimization
+from simulated_annealing import SimulatedAnnealingOptimization
+
+fpe = pd.read_csv(home_dir + '\\data\\preprocessed\\FPE.csv', sep=';', index_col='Data')['Total']
+
+# Endog
+fpe_1m = fpe.loc[fpe.index < fpe.index[-11]]
+fpe_6m = fpe.loc[fpe.index < fpe.index[-6]]
+fpe_12m = fpe
+
+
+exec_log = []
+
+#Metodo que vai calcular o Erro do Modelo-SARIMAX
+def calculo_erro_sarimax(desp_serie, param, intervalo):
+    try:
+        arr_real =[]
+        inter_real = []
+        for i in range(intervalo):
+            max_size = max(desp_serie.index)
+            arr_real.append(desp_serie[max_size])
+            inter_real.append(max_size)
+            desp_serie = desp_serie.drop(max_size)
+    
+        model_fit = SARIMAX_Constructor(desp_serie, exec_log).fit_model(param)
+    
+        start = len(desp_serie)
+        end = len(desp_serie)+intervalo-1
+    
+        exec_log.append(('P1', param, 'Done', None))
+        previsto = model_fit.predict(start = start, end = end)
+        exec_log.append(('P2', param, 'Done', None))
+        serie_real = pd.Series(arr_real, inter_real)
+    
+        size = len(previsto)
+        erro = []
+    
+        for i in range(size):
+            parc_erro = (previsto[i] - serie_real.iloc[i])/serie_real.iloc[i]
+            erro.append(abs(parc_erro))
+    
+        erro_medio = np.mean(erro)
+        return abs(erro_medio)
+    except Exception as e:
+        exec_log.append(('E', param, str(e), None))
+        return 99999
+
+#Metodo que vai calcular o Erro do Modelo Holt-Winters
+def calculo_erro_holt(desp_serie, param, intervalo):
+    arr_real =[]
+    inter_real = []
+    for i in range(intervalo):
+        max_size = max(desp_serie.index)
+        arr_real.append(desp_serie[max_size])
+        inter_real.append(max_size)
+        desp_serie = desp_serie.drop(max_size)
+
+
+    holt_const = Holt_Constructor()
+    model_fit = holt_const.create_Holt_Winters(desp_serie, param)
+
+    start = len(desp_serie)
+    end = len(desp_serie)+intervalo-1
+
+    previsto = model_fit.predict(start = start, end = end)
+    serie_real = pd.Series(arr_real, inter_real)
+
+    size = len(previsto)
+    erro = []
+
+    for i in range(size):
+        parc_erro = (previsto[i] - serie_real.iloc[i])/serie_real.iloc[i]
+        erro.append(abs(parc_erro))
+
+    erro_medio = np.mean(erro)
+    return abs(erro_medio)
+                
+
+warnings.filterwarnings('ignore')
+
+# =============================================================================
+# Hill Climb
+# =============================================================================
+# 1 month
+
+intervalo = 1
+
+hill_holts = HillClimbOptimization(calculo_erro_holt, 29)
+hill_sarimax = HillClimbOptimization(calculo_erro_sarimax, 26)
+
+hill_holts.start_random_initialization(25, intervalo, fpe_1m)
+hill_sarimax.start_random_initialization(25, intervalo, fpe_1m)
+
+test_results_holt, log_holts = hill_holts.optimize(1000, intervalo, fpe_1m)
+test_results_sarimax, log_sarimax = hill_sarimax.optimize(100, intervalo, fpe_1m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_hill_climb_1m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_hill_climb_1m.csv', sep=';')
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_hill_climb_1m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_hill_climb_1m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt1.csv', sep=';')
+
+# =============================================================================
+# 6 months
+
+intervalo = 6
+
+hill_holts = HillClimbOptimization(calculo_erro_holt, 29)
+hill_sarimax = HillClimbOptimization(calculo_erro_sarimax, 26)
+
+hill_holts.start_random_initialization(25, intervalo, fpe_6m)
+hill_sarimax.start_random_initialization(25, intervalo, fpe_6m)
+
+test_results_holt, log_holts = hill_holts.optimize(1000, intervalo, fpe_6m)
+test_results_sarimax, log_sarimax = hill_sarimax.optimize(100, intervalo, fpe_6m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_hill_climb_6m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_hill_climb_6m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt2.csv', sep=';')
+
+# =============================================================================
+# 12 months
+
+intervalo = 12
+
+hill_holts = HillClimbOptimization(calculo_erro_holt, 29)
+hill_sarimax = HillClimbOptimization(calculo_erro_sarimax, 26)
+
+hill_holts.start_random_initialization(25, intervalo, fpe_12m)
+hill_sarimax.start_random_initialization(25, intervalo, fpe_12m)
+
+test_results_holt, log_holts = hill_holts.optimize(1000, intervalo, fpe_12m)
+test_results_sarimax, log_sarimax = hill_sarimax.optimize(100, intervalo, fpe_12m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_hill_climb_12m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_hill_climb_12m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt3.csv', sep=';')
+
+# =============================================================================
+# Simulated Annealing
+# =============================================================================
+# 1 month
+
+intervalo = 1
+
+anneal_holts = SimulatedAnnealingOptimization(calculo_erro_holt, 29)
+anneal_sarimax = SimulatedAnnealingOptimization(calculo_erro_sarimax, 26)
+
+anneal_holts.start_random_initialization(25, intervalo, fpe_1m)
+anneal_sarimax.start_random_initialization(25, intervalo, fpe_1m)
+
+test_results_holt, log_holts = anneal_holts.optimize(1000, intervalo, fpe_1m)
+test_results_sarimax, log_sarimax = anneal_sarimax.optimize(100, intervalo, fpe_1m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_simulated_annealing_1m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_simulated_annealing_1m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt4.csv', sep=';')
+
+# =============================================================================
+# 6 months
+
+intervalo = 6
+
+anneal_holts = SimulatedAnnealingOptimization(calculo_erro_holt, 29)
+anneal_sarimax = SimulatedAnnealingOptimization(calculo_erro_sarimax, 26)
+
+anneal_holts.start_random_initialization(25, intervalo, fpe_6m)
+anneal_sarimax.start_random_initialization(25, intervalo, fpe_6m)
+
+test_results_holt, log_holts = anneal_holts.optimize(1000, intervalo, fpe_6m)
+test_results_sarimax, log_sarimax = anneal_sarimax.optimize(100, intervalo, fpe_6m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_simulated_annealing_6m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_simulated_annealing_6m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt5.csv', sep=';')
+
+# =============================================================================
+# 12 months
+
+intervalo = 12
+
+anneal_holts = SimulatedAnnealingOptimization(calculo_erro_holt, 29)
+anneal_sarimax = SimulatedAnnealingOptimization(calculo_erro_sarimax, 26)
+
+anneal_holts.start_random_initialization(25, intervalo, fpe_12m)
+anneal_sarimax.start_random_initialization(25, intervalo, fpe_12m)
+
+test_results_holt, log_holts = anneal_holts.optimize(1000, intervalo, fpe_12m)
+test_results_sarimax, log_sarimax = anneal_sarimax.optimize(100, intervalo, fpe_12m)
+
+log_holts.to_csv(home_dir + '\\data\\logs\\log_holts_simulated_annealing_12m.csv', sep=';')
+log_sarimax.to_csv(home_dir + '\\data\\logs\\log_sarimax_simulated_annealing_12m.csv', sep=';')
+
+df_log = pd.DataFrame(exec_log)
+df_log.to_csv(home_dir + '\\data\\logs\\exec_logs\\exec_log_pt6.csv', sep=';')
--- classes/simulated_annealing.py
+++ classes/simulated_annealing.py
@@ -13,13 +13,14 @@ home_dir = 'E:\\leoml\\Documents\\Leonardo\\SEFAZ\\FPE\\Time Series Forecast - F
 sys.path.append(home_dir)
 
 import pandas as pd
+import random
 from random_binary_generator import RandomBinaryGenerator
 
 class SimulatedAnnealingOptimization(object):
     
-    def __init__(self):
-        self.generator = RandomBinaryGenerator(29)
-        self.log_holts = {
+    def __init__(self, f_calcular_erro, bits):
+        self.generator = RandomBinaryGenerator(bits)
+        self.log = {
             'ID': [],
             'ITERACAO': [],
             'PARAM_ATUAL': [],
@@ -27,64 +28,68 @@ class SimulatedAnnealingOptimization(object):
             'PARAM_MELHOR': [],
             'MPA_MELHOR': []
         }
-        self.test_results_holt = {
+        self.test_results = {
             'ID': [],
             'ITERACOES': [],
             'PARAM': [],
             'MPA': []
         }
+        self.f_calcular_erro = f_calcular_erro
     
-    def start_random_initialization(self, n, intevalo):
+    def start_random_initialization(self, n, intevalo, fpe):
         for i in range(n):
+            print('init: ', i)
             param = self.generator.random_binary()
             
-            erro_medio = calculo_erro_holt(fpe, param, 1)
+            erro_medio = self.f_calcular_erro(fpe, param, 1)
             
-            self.test_results_holt['ID'].append(i)
-            self.test_results_holt['ITERACOES'].append(1)
-            self.test_results_holt['PARAM'].append(param)
-            self.test_results_holt['MPA'].append(erro_medio)
+            self.test_results['ID'].append(i)
+            self.test_results['ITERACOES'].append(1)
+            self.test_results['PARAM'].append(param)
+            self.test_results['MPA'].append(erro_medio)
             
             self.log_holts = {
-                'ID': self.test_results_holt['ID'].copy(),
-                'ITERACAO': self.test_results_holt['ITERACOES'].copy(),
-                'PARAM_ATUAL': self.test_results_holt['PARAM'].copy(),
-                'MPA_ATUAL': self.test_results_holt['MPA'].copy(),
-                'PARAM_MELHOR': self.test_results_holt['PARAM'].copy(),
-                'MPA_MELHOR': self.test_results_holt['MPA'].copy()
+                'ID': self.test_results['ID'].copy(),
+                'ITERACAO': self.test_results['ITERACOES'].copy(),
+                'PARAM_ATUAL': self.test_results['PARAM'].copy(),
+                'MPA_ATUAL': self.test_results['MPA'].copy(),
+                'PARAM_MELHOR': self.test_results['PARAM'].copy(),
+                'MPA_MELHOR': self.test_results['MPA'].copy()
             }
     
-    def optimize(self, max_iter, intervalo):
-        for i in range(len(self.test_results_holt['ITERACOES'])):
+    def optimize(self, max_iter, intervalo, fpe):
+        for i in range(len(self.test_results['ITERACOES'])):
             print('Iteração: ', i + 1)
             
-            while self.test_results_holt['ITERACOES'][i] < max_iter:
-                self.test_results_holt['ITERACOES'][i] += 1
+            while self.test_results['ITERACOES'][i] < max_iter:
+                self.test_results['ITERACOES'][i] += 1
                 
-                id_ = self.test_results_holt['ID'][i]
-                param = self.generator.simple_binary_tweak(self.test_results_holt['PARAM'][i])
+                if self.test_results['ITERACOES'][i] % 10 == 0: print('i: ', self.test_results['ITERACOES'][i])
                 
-                erro_medio = calculo_erro_holt(fpe, param, intervalo)
+                id_ = self.test_results['ID'][i]
+                param = self.generator.simple_binary_tweak(self.test_results['PARAM'][i])
                 
-                self.log_holts['ID'].append(id_)
-                self.log_holts['ITERACAO'].append(self.test_results_holt['ITERACOES'][i])
-                self.log_holts['PARAM_ATUAL'].append(param)
-                self.log_holts['MPA_ATUAL'].append(erro_medio)
+                erro_medio = self.f_calcular_erro(fpe, param, intervalo)
                 
-                if erro_medio <= self.test_results_holt['MPA'][i]:
+                self.log['ID'].append(id_)
+                self.log['ITERACAO'].append(self.test_results['ITERACOES'][i])
+                self.log['PARAM_ATUAL'].append(param)
+                self.log['MPA_ATUAL'].append(erro_medio)
+                
+                if erro_medio <= self.test_results['MPA'][i]:
                     
-                    self.test_results_holt['ID'][i] = id_
-                    self.test_results_holt['PARAM'][i] = param
-                    self.test_results_holt['MPA'][i] = erro_medio
+                    self.test_results['ID'][i] = id_
+                    self.test_results['PARAM'][i] = param
+                    self.test_results['MPA'][i] = erro_medio
                 else:
-                    p = 1/ (self.test_results_holt['ITERACOES'][i])
+                    p = 1/ (self.test_results['ITERACOES'][i])
                     
                     if random.random() < p:
-                        self.test_results_holt['ID'][i] = id_
-                        self.test_results_holt['PARAM'][i] = param
-                        self.test_results_holt['MPA'][i] = erro_medio
+                        self.test_results['ID'][i] = id_
+                        self.test_results['PARAM'][i] = param
+                        self.test_results['MPA'][i] = erro_medio
                         
-                self.log_holts['PARAM_MELHOR'].append(self.test_results_holt['PARAM'][i])
-                self.log_holts['MPA_MELHOR'].append(self.test_results_holt['MPA'][i])
+                self.log['PARAM_MELHOR'].append(self.test_results['PARAM'][i])
+                self.log['MPA_MELHOR'].append(self.test_results['MPA'][i])
                     
-        return pd.DataFrame(self.test_results_holt), pd.DataFrame(self.log_holts)
+        return pd.DataFrame(self.test_results), pd.DataFrame(self.log)
